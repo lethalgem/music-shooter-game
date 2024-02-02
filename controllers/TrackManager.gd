77fc5feffdbbb8708fe5c6b42d2821b91b540837
lethalgem <iwc123@gmail.com>
2024-02-01 21:03:40 -0600

    Swap tracks on shoot

---
+++ controllers/TrackManager.gd
@@ -0,0 +1,24 @@
+class_name TrackManager extends Node
+
+@export var FRONT_TRACK_STREAM_PLAYER: AudioStreamPlayer
+@export var BACKING_TRACK_STREAM_PLAYER: AudioStreamPlayer
+
+
+func _ready():
+	BACKING_TRACK_STREAM_PLAYER.play()
+
+
+func _input(event):
+	if Input.is_action_just_pressed("shoot"):
+		print("pressed")
+		var current_playback_position = BACKING_TRACK_STREAM_PLAYER.get_playback_position()
+		FRONT_TRACK_STREAM_PLAYER.play(current_playback_position)
+		BACKING_TRACK_STREAM_PLAYER.stop()
+		print(FRONT_TRACK_STREAM_PLAYER.get_playback_position())
+	elif Input.is_action_just_released("shoot"):
+		print("released")
+		var current_playback_position = FRONT_TRACK_STREAM_PLAYER.get_playback_position()
+		print(current_playback_position)
+		BACKING_TRACK_STREAM_PLAYER.play(current_playback_position)
+		FRONT_TRACK_STREAM_PLAYER.stop()
+		print(BACKING_TRACK_STREAM_PLAYER.get_playback_position())
