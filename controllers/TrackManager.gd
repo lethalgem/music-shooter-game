e19c692ef9be93462ed5ea17c6be8f860382441a
lethalgem <iwc123@gmail.com>
2024-02-03 23:31:39 -0600

    Shoot according to shoot times in file

--- controllers/TrackManager.gd
+++ controllers/TrackManager.gd
@@ -2,30 +2,39 @@ class_name TrackManager extends Node
 
 @export var FRONT_TRACK_STREAM_PLAYER: AudioStreamPlayer
 @export var BACKING_TRACK_STREAM_PLAYER: AudioStreamPlayer
+@export var SHOOT_TIMES_SOURCE: String
 
 signal shoot
 
 var released: bool = true
-var shoot_times: Array[int] = []
+var shoot_times = []
+var current_shot_index: int = 0
 
 
 func _ready():
-	var interval = 236000 / 2240
-	for i in range(1, 2240):
-		shoot_times.append((interval * i) / 10)
+	load_file()
+	BACKING_TRACK_STREAM_PLAYER.play(0)
 
-	BACKING_TRACK_STREAM_PLAYER.play()
+
+func load_file():
+	var file = FileAccess.open(SHOOT_TIMES_SOURCE, FileAccess.READ)
+	shoot_times = str_to_var(file.get_as_text())
 
 
 func _process(delta):
 	var current_song_millis: int
 	if released:
-		current_song_millis = int(BACKING_TRACK_STREAM_PLAYER.get_playback_position() * 1000) / 10
+		current_song_millis = int(BACKING_TRACK_STREAM_PLAYER.get_playback_position() * 1000)
 	else:
-		current_song_millis = int(FRONT_TRACK_STREAM_PLAYER.get_playback_position() * 1000) / 10
+		current_song_millis = int(FRONT_TRACK_STREAM_PLAYER.get_playback_position() * 1000)
 
-	if shoot_times.has(current_song_millis) and !released:
+	if (
+		current_shot_index < shoot_times.size()
+		and current_song_millis >= shoot_times[current_shot_index]
+		and !released
+	):
 		shoot.emit()
+		current_shot_index += 1
 
 
 func _unhandled_input(event):
