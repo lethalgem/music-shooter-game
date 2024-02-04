14f7ebe1aaf4c0dba7a28da43b2b44f022c6aabd
lethalgem <iwc123@gmail.com>
2024-02-03 21:20:20 -0600

    Add a crappy physical flash

--- scripts/muzzle_flash.gd
+++ scripts/muzzle_flash.gd
@@ -1,8 +1,10 @@
-class_name MuzzleFlash extends OmniLight3D
+class_name MuzzleFlash extends Node3D
 
 @export var track_manager: TrackManager
 @export var max_flash: float = 9
-@export var flash_duration: float = 0.1
+@export var flash_duration: float = 0.05
+@export var muzzle_flash_light: OmniLight3D
+@export var max_flash_size: Vector3 = Vector3(1, 1, 1)
 
 
 func _ready():
@@ -10,7 +12,9 @@ func _ready():
 
 
 func flash():
-	var flash_tween = create_tween()
-	flash_tween.tween_property(self, "light_energy", max_flash, flash_duration)
+	var flash_tween = create_tween().parallel()
+	flash_tween.tween_property(muzzle_flash_light, "light_energy", max_flash, flash_duration)
+	flash_tween.tween_property(self, "scale", max_flash_size, flash_duration)
 	await flash_tween.finished
-	light_energy = 0
+	muzzle_flash_light.light_energy = 0
+	scale = Vector3.ZERO
