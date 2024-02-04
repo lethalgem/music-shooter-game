7c14be6e6044318c0190aecd4fc1ede00fd6ac71
lethalgem <iwc123@gmail.com>
2024-02-03 20:16:50 -0600

    shoot multiple times while holding

--- controllers/TrackManager.gd
+++ controllers/TrackManager.gd
@@ -6,10 +6,27 @@ class_name TrackManager extends Node
 signal shoot
 
 var released: bool = true
+var shoot_times: Array[int] = []
 
 
 func _ready():
+	var interval = 236000 / 2240
+	for i in range(1, 2240):
+		shoot_times.append((interval * i) / 10)
+	print(shoot_times)
 	BACKING_TRACK_STREAM_PLAYER.play()
+
+
+func _process(delta):
+	var current_song_millis: int
+	if released:
+		current_song_millis = int(BACKING_TRACK_STREAM_PLAYER.get_playback_position() * 1000) / 10
+	else:
+		current_song_millis = int(FRONT_TRACK_STREAM_PLAYER.get_playback_position() * 1000) / 10
+
+	print(current_song_millis)
+	if shoot_times.has(current_song_millis) and !released:
+		shoot.emit()
 
 
 func _unhandled_input(event):
@@ -19,7 +36,6 @@ func _unhandled_input(event):
 			var current_playback_position = BACKING_TRACK_STREAM_PLAYER.get_playback_position()
 			FRONT_TRACK_STREAM_PLAYER.play(current_playback_position)
 			BACKING_TRACK_STREAM_PLAYER.stop()
-			shoot.emit()
 			released = false
 	elif Input.is_action_just_released("shoot"):
 		print("released")
