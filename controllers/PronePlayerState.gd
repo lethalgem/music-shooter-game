c4fa031b6505cea24e0cfbc9f49249fd03e55f14
ChapoTheGoat <gama_giovanni@yahoo.com>
2024-02-08 20:07:27 -0600

    Resolve_comments

--- controllers/PronePlayerState.gd
+++ controllers/PronePlayerState.gd
@@ -11,11 +11,7 @@ var RELEASED: bool = false
 
 func enter(previous_state) -> void:
 	ANIMATION.speed_scale = 1.0
-	#if previous_state.name != "SlidingPlayerState":
-		#ANIMATION.play("Crouching", -1.0, PRONE_SPEED)
 	if previous_state.name == "IdlePlayerState":
-		#pass
-		#ANIMATION.current_animation = "Proning"
 		ANIMATION.play("Proning", -1.0, PRONE_SPEED)
 
 
@@ -29,21 +25,9 @@ func update(delta):
 
 	if Input.is_action_just_pressed("prone"):
 		unprone()
-	#elif Input.is_action_pressed("crouch") == false and RELEASED == false:  # When not holding crouch, set crouch_released to true. Then trigger unprone.
-		#RELEASED = true
-		#unprone()
-
 
 func unprone():
 	ANIMATION.play("Proning",-1.0,-PRONE_SPEED*1.4,true)
 	if ANIMATION.is_playing():
 		await ANIMATION.animation_finished
 	transition.emit("IdlePlayerState")
-	#if CROUCH_SHAPECAST.is_colliding() == false and Input.is_action_pressed("crouch") == false:
-		#ANIMATION.play("Crouching", -1.0, -PRONE_SPEED * 1.5, true)
-		#if ANIMATION.is_playing():
-			#await ANIMATION.animation_finished
-		#transition.emit("IdlePlayerState")
-	#elif CROUCH_SHAPECAST.is_colliding() == true:
-		#await get_tree().create_timer(0.1).timeout
-		#unprone()
